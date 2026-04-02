4700d6359c40a0d3a67dabf4fac5de6cae7629c0
ejdam87 <adam.dzadon@gmail.com>
2026-04-02 14:02:39 +0200

    feat: dataset labels handling

--- src/DET_524839/dataset.py
+++ src/DET_524839/dataset.py
@@ -1,24 +1,49 @@
 # STUDENT's UCO: 524839
 
+from abc import ABC, abstractmethod
 from pathlib import Path
+from typing import TypeVar, Generic, Any
 
 import torch
 import numpy as np
 import pandas as pd
+from PIL import Image
 from torch import Tensor
 from torch.utils.data import Dataset
 from albumentations import ToTensorV2
 
-Image = Tensor  # (3, H, W)
+ImageSample = Tensor  # (3, H, W)
 Bboxes = Tensor # (N, 4)
 
-Metadata = dict[str, str]
+Metadata = dict[str, Any]
 
-LabeledSample = tuple[Image, Metadata, Bboxes]
-UnlabeledSample = tuple[Image, Metadata]
+LabeledSample = tuple[ImageSample, Metadata, Bboxes]
+UnlabeledSample = tuple[ImageSample, Metadata]
 
+T = TypeVar("T")
 
-class LabeledDetectionDataset(Dataset[LabeledSample]):
+class DetectionDataset(ABC, Dataset[T], Generic[T]):
+
+    def __init__(self, image_paths: list[Path]) -> None:
+        super().__init__()
+
+        self.image_paths = image_paths
+        self.to_tensor = ToTensorV2()
+
+    def __len__(self) -> int:
+        return len(self.image_paths)
+
+    def _get_image(self, idx: int) -> Tensor:
+        img = Image.open(str(self.image_paths[idx]))
+        img = np.array(img)
+        img = self.to_tensor(image=img)["image"]
+        return img
+
+    @abstractmethod
+    def __getitem__(self, idx: int) -> T:
+        pass
+
+class LabeledDetectionDataset(DetectionDataset[LabeledSample]):
 
     def __init__(
             self,
@@ -27,19 +52,12 @@ class LabeledDetectionDataset(Dataset[LabeledSample]):
         ) -> None:
 
         assert len(image_paths) == len(bbox_paths), "Expecting same amount of images and labels"
-        super().__init__()
+        super().__init__(image_paths=image_paths)
 
-        self.image_paths = image_paths
         self.bbox_paths = bbox_paths
-        self.to_tensor = ToTensorV2()
-
-    def __len__(self) -> int:
-        return len(self.image_paths)
 
     def __getitem__(self, idx: int) -> LabeledSample:
-        img = Image.open(str(self.image_paths[idx]))
-        img = np.array(img)
-        img = self.to_tensor(image=img)["image"]
+        img = self._get_image(idx)
 
         bbox_df = pd.read_csv(str(self.bbox_paths[idx]))
         bbox_torch = torch.tensor(bbox_df[["xmin", "ymin", "xmax", "ymax"]].values, dtype=torch.float32)
@@ -50,3 +68,14 @@ class LabeledDetectionDataset(Dataset[LabeledSample]):
         }
 
         return img, metadata, bbox_torch
+
+
+class UnlabeledDetectionDataset(DetectionDataset[UnlabeledSample]):
+
+    def __getitem__(self, idx: int) -> UnlabeledSample:
+        img = self._get_image(idx)
+        metadata = {
+            "filename": self.image_paths[idx].name,
+        }
+
+        return img, metadata
